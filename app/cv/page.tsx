50417767e94bea3c6a56ff45f02bbe96f0296262
QueAnka <annika.nylin@hotmail.com>
2025-02-28 17:20:28 +0100

    adjusted mobile view

--- app/cv/page.tsx
+++ app/cv/page.tsx
@@ -8,7 +8,7 @@ const CvPage = () => {
         <div className='pb-32 pt-20 min-h-svh'>
             <h1 className='w-full text-center text-green pb-16'>CV</h1>
             <div className='w-full mx-auto'>
-                <ul className='flex flex-wrap gap-16 justify-center mx-auto px-8 w-fit max-w-screen-lg '>
+                <ul className='flex flex-col gap-16 justify-center mx-auto px-8 w-fit max-w-screen-lg '>
                     {data.map((item) => (
                         <CVItems key={item.heading} {...item} />
                     ))}
@@ -28,13 +28,14 @@ interface CVItemsProps {
         place?: string;
         description?: string;
         link?: string;
+        linkName?: string;
     }[];
 }
 
 const CVItems = ({ heading, item }: CVItemsProps) => {
     const [show, setShow] = useState(false);
 
-    const showClass = show ? "block" : "hidden";  
+    const showClass = show ? "block" : "hidden";
 
     const open = () => {
         setShow(!show);
@@ -42,7 +43,7 @@ const CVItems = ({ heading, item }: CVItemsProps) => {
 
     return (
         <li className='flex-1 flex flex-col justify-center items-center text-green'>
-            <div className='w-96 sm:w-[600px] h-fit p-4 rounded-md border border-gray-500'>
+            <div className='min-w-72 w-80 md:w-96 sm:max-w-[600px] h-fit p-4 rounded-md border border-gray-500'>
                 <div
                     onClick={open}
                     className='cursor-pointer w-full flex justify-between items-center gap-3'
@@ -64,17 +65,17 @@ const CVItems = ({ heading, item }: CVItemsProps) => {
                                 </h4>
 
                                 <span className='inline-block w-px bg-green'></span>
-                                <p className="text-nowrap">{item.year}</p>
+                                <p className='text-nowrap'>{item.year}</p>
                             </div>
                             <p className=' text-purple'>{item.place}</p>
                             <p className=' text-purple'>{item.description}</p>
                             <a
                                 href={item.link}
                                 target='_blank'
-                                className='h-4 text-blue'
+                                className='h-4 text-blue max-w-full text-wrap'
                             >
-                                {item.link}
-                            </a>
+                                {item.linkName}
+                            </a> 
                         </li>
                     ))}
                 </ul>
@@ -165,10 +166,12 @@ const data = [
             {
                 title: "LinkedIn",
                 link: "https://www.linkedin.com/in/annika-nylin",
+                linkName: "Annika Nylin",
             },
             {
                 title: "Github",
                 link: "https://github.com/QueAnkan",
+                linkName: "QueAnkan",
             },
         ],
     },
